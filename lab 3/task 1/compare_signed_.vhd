66ede14badcf8f3cb2a3496616149d631e0dd2f6
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-10-11 10:59:07 -0500

    FIGURED OUT prelab 3 and starting writing the code for it

---
+++ lab 3/task 1/compare_signed_.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity compare_signed is --start entity declaration statement
+
+port( 
+		signal A: in std_logic_vector(1 downto 0); --signal name : std_logic_vector(highint downto lowint);
+		signal B: in std_logic_vector(1 downto 0); --signal name : std_logic_vector(highint downto lowint);
+		signal L: out std_logic; --signal_name : signal_mode data_type 
+		signal E: out std_logic; --signal_name : signal_mode data_type 
+		signal G: out std_logic	 --signal_name : signal_mode data_type 
+	);--end of port clause
+
+	
+end entity; --end entity declaration statement
+
+	
+architecture mixed of compare_signed	--start of architecture declaration statement 
+	
+													--describes what the entity does 
+													--declaration potion of the architecture
