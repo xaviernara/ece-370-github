4c86e4914a981175fe8da8cf861c277d56790963
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-11-01 10:53:22 -0500

    still trying to fix task 1 and 2

--- lab 6/task 1/arbiter_lsb_nbit.vhd
+++ lab 6/task 1/arbiter_lsb_nbit.vhd
@@ -17,34 +17,36 @@ end arbiter_lsb_nbit;
 
 architecture dataflow of arbiter_lsb_nbit is
 
-	signal zero : std_logic_vector(N downto 0);
+	signal zero : std_logic_vector(N-1 downto 0);
 
 begin
 --single concurrent for...generate statement in combination with
---concurrent “simple” signal assignment (CSSA) statements to model the
+--concurrent “simple” signal assignment (CSSA) (boolean logic) statements to model the
 --i-th bit of the zero prefix indicator that spans bit positions 0 to i-1, for i > 0.
 
-	ARBITER: for i in 0 to N-1 generate
-		z(i)<= (req(i) not zero(i))  and grant(i);
-	
-	
-	<optional_label>: <target> <= 
-	<value> when <condition> else
-	<value> when <condition> else 
-	<value> when <condition> else
-	...
-	OTHERS;
-end generate ARBITER;
+	 zero(0)<=(not req(0));
 
-
-	<optional_label>: <target> <= 
-	<value> when <condition> else
-	<value> when <condition> else 
-	<value> when <condition> elsE
-	...
-	<value>;
+	ARBITER: for i in 1 to N-1 generate
+		--z(i)<= (req(i) not zero(i))  and grant(i);
+		grant(i)<= (not req(i) and  zero(i-1)); 
 	
-	z(N-1)<= (req(N-1) not zero(N))  and grant(N-1);
+	   end generate ARBITER;
+		
+	--concurrent “simple” signal  assignment  (CSSA) means boolean logic 
+
+	grant(0)<= req(0);
+
+	 --grant(0)<= req(0) when '0' else
+					--req(N-1);
+					
+			  
+	
+	
+	--vector slicing to assign the Z port signal
+	--z<= (req(N-1) not zero(N))  and grant(N-1); <=wrong
+	
+	Z<= zero(N-1); 
+	
 end dataflow;
 
 
