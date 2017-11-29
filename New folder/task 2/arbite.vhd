1429fa27eff825be1ec79e16918fee08d814f012
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-11-29 17:01:35 -0600

    starting task 2

--- New folder/task 2/arbite.vhd
+++ New folder/task 2/arbite.vhd
@@ -4,26 +4,45 @@ use ieee.std_logic_1164.all;
 entity arbiter_controller is
 
 	port(
-		clk : in	std_logic;
-		R	 : in	std_logic_VECTOR(0 TO 2);
-		rst : in	std_logic;
-		G	 : out	std_logic_vector(0 to 2)
+		clk : in	std_logic;  --Asynchronous reset
+		R	 : in	std_logic_VECTOR(0 TO 2); --Synchronizing clock signa
+		rst : in	std_logic; --Device request signals
+		G	 : out	std_logic_vector(0 to 2) --Device grant signals
 	);
 
 end entity;
+
+--Use coding style Triple-Segment-Style for this implementation.  
 
 architecture behavior of arbiter_controlleris
 
 	-- Build an enumerated type for the state machine
 	--These names represent the states of the bus arbiter state diagram
 	type StateType is (IDLE,GNT_DEV0 , GNT_DEV1,GNT_DEV2) ;
-	signal presentState : StateType;
+	signal presentState, nextState : StateType;
 
 
 begin
 
+Utilize  a 
+concurrent  sensitivity
+-
+list 
+process
+statement
+to 
+model the 
+next  state
+combinational 
+logic
+of  the  bus  arbiter  controller.   
+Which  signal(s)  belong  in  the 
+sensitivity list?
+Name/label this process statement next_state_logic
+
+
 	-- Logic to advance to the next state
-	process (clk, rst)
+arbiter_controller:process (clk, rst)
 	begin
 	
 		if rst = '1' then
@@ -63,24 +82,22 @@ begin
 					if R(2) = '0' then presentState <= IDLE;
 					--elsif R(2) ='1' then presentState <= GNT_DEV2 ;
 					else	presentState <= GNT_DEV2 ;
+
 					end if;
 					
+				when others=> presentState <=IDLE; 
 					
 			end case;
 		end if;
-	end process;
+	end process arbiter_controller;
+	
+	
+--Utilize a single concurrent conditional signal assignment (CCSA) 
+--statement to model the combinational output logic of the bus arbiter controller
 
-	-- Logic to determine output
-	process (state)
-	begin
-		case state is
-			when s0 =>
-				output <= "00";
-			when s1 =>
-				output <= "01";
-			when s2 =>
-				output <= "10";
-		end case;
-	end process;
+--G <= 
 
-end rtl;
+	
+	
+
+end architecture behavior;
